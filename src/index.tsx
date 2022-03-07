8a0c0688b708526a3e8b4dbffe747302d59e4757
Noa <juliiip211@gmail.com>
2022-03-07 19:55:13 +0800

    [docs] update index

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,6 @@ import './index.less';
 export { default as Portal } from './portal';
 export { default as Popup } from './popup';
 
-const Popup = () => (
-  <div className="react-easy-popup">hello,react-easy-popup</div>
-);
+const Popup = () => <div className="react-easy-popup"></div>;
 
 export default Popup;
